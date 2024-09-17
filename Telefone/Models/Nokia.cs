c95dad2f5ee1b9a49adee309e5264c2fd27c3ee3
Kaio Gabriel <137839907+kaiogabriel10@users.noreply.github.com>
2024-09-17 16:22:45 -0300

    Add files via upload

---
+++ Telefone/Models/Nokia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefone.Models
+{
+    internal class Nokia : SmartPhone
+    {
+        public Nokia(int numero, string modelo, int memoria, int Imei) : base(numero, modelo, memoria, Imei)
+        {
+            this.numero = numero;
+            this.modelo = modelo;
+            this.memoria = memoria;
+            this.Imei = Imei;
+        }
+
+        public override void InstalarApp()
+        {
+            Console.WriteLine("Digite o nome do App.");
+            string app = Console.ReadLine();
+            Console.WriteLine($"Instalando o App {app}");
+        }
+    }
+}
---
+++ Telefone/Models/SmartPhone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefone.Models
+{
+    internal abstract class SmartPhone
+    {
+        public int numero;
+        public string modelo;
+        public int memoria;
+        public int Imei;
+
+        public SmartPhone(int numero, string modelo, int memoria, int imei)
+        {
+            this.numero = numero;
+            this.modelo = modelo;
+            this.memoria = memoria;
+            this.Imei = imei;
+        }
+
+        public virtual void Ligar()
+        {
+            Console.WriteLine("Digite o número:");
+            string number = Console.ReadLine();
+            int num = int.Parse(number);
+
+            Console.WriteLine($"Ligando para {num}");
+
+        }
+        public virtual void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo Ligação");
+        }
+
+        public abstract void InstalarApp();
+    }
+}
